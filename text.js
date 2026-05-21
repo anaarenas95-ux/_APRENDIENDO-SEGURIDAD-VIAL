dc4901be4bfd8a3da9b022808f71c39752b2d59a
Ana María Pareja Pérez <ana.arenas95@gmail.com>
2026-05-21 13:21:04 +0200

    Add files via upload

---
+++ text.js
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="es" id="exe-page-mpfefbr1-3mjjigtqn">
+<head>
+<meta charset="utf-8">
+<meta name="generator" content="eXeLearning v4.0.0">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="license" type="text/html" href="https://creativecommons.org/licenses/by-sa/4.0/">
+<title>SESIÓN 4 | Aprendiendo Seguridad Vial</title>
+<link rel="icon" type="image/x-icon" href="../libs/favicon.ico">
+<meta name="description" content="Este proyecto se centra en la educación vial como herramienta fundamental para potenciar la seguridad personal y la autonomía de niños y niñas con necesidades educativas especiales. A lo largo de cuatro semanas, se llevarán a cabo sesiones de una hora dos veces a la semana, donde se abordarán conceptos básicos sobre circulación peatonal, señales de tráfico y seguridad en el automóvil.">
+<script>document.querySelector("html").classList.add("js");</script><script src="../libs/jquery/jquery.min.js"> </script><script src="../libs/common_i18n.js"> </script><script src="../libs/common.js"> </script><script src="../libs/exe_export.js"> </script><script src="../libs/bootstrap/bootstrap.bundle.min.js"> </script><link rel="stylesheet" href="../libs/bootstrap/bootstrap.min.css">
+<script src="../idevices/text/text.js"></script><link rel="stylesheet" href="../idevices/text/text.css">
+<link rel="stylesheet" href="../content/css/base.css"><script src="../theme/style.js"> </script><link rel="stylesheet" href="../theme/style.css">
+</head>
+<body class="exe-export exe-web-site">
+<script>document.body.className+=" js"</script>
+<div class="exe-content exe-export pre-js siteNav-hidden"><a href="#page-mpfefbr1-3mjjigtqn" id="skipNav">Skip to content</a> <nav id="siteNav">
+<ul>
+<li> <a href="../index.html" class="main-node no-ch">PROYECTO FINAL</a>
+</li>
+<li> <a href="../html/introduccion.html" class="no-ch">INTRODUCCIÓN</a>
+</li>
+<li class="current-page-parent"> <a href="../html/sesiones-de-trabajo.html" class="daddy">SESIONES DE TRABAJO</a>
+<ul class="other-section">
+<li> <a href="../html/sesion-1.html" class="no-ch">SESIÓN 1</a>
+</li>
+<li> <a href="../html/sesion-2.html" class="no-ch">SESIÓN 2</a>
+</li>
+<li> <a href="../html/sesion-3.html" class="no-ch">SESIÓN 3</a>
+</li>
+<li class="active"> <a href="../html/sesion-4.html" class="active no-ch">SESIÓN 4</a>
+</li>
+<li> <a href="../html/sesion-5.html" class="no-ch">SESIÓN 5</a>
+</li>
+<li> <a href="../html/sesion-6.html" class="no-ch">SESIÓN 6</a>
+</li>
+<li> <a href="../html/sesion-7.html" class="no-ch">SESIÓN 7</a>
+</li>
+</ul>
+</li>
+<li> <a href="../html/guia-didactica.html" class="no-ch">GUÍA DIDÁCTICA</a>
+</li>
+<li> <a href="../html/evaluacion.html" class="no-ch">EVALUACIÓN</a>
+</li>
+<li> <a href="../html/creditos.html" class="no-ch">CRÉDITOS</a>
+</li>
+</ul>
+</nav><main id="page-mpfefbr1-3mjjigtqn" class="page"> 
+<header class="main-header">
+<div class="package-header"><p class="package-title">Aprendiendo Seguridad Vial</p>
+<p class="package-subtitle">Un viaje Divertido y Seguro</p></div>
+<div class="page-header"><h1 class="page-title">SESIÓN 4</h1></div>
+</header><div id="page-content-page-mpfefbr1-3mjjigtqn" class="page-content">
+<article id="block-1778594720031-bmpiiul8a" class="box">
+<header class="box-head no-icon">
+<h1 class="box-title">Título: Visualización y discusión de vídeo</h1>
+<button class="box-toggle box-toggle-on" title="Toggle content">
+<span>Toggle content</span>
+</button></header>
+<div class="box-content">
+<div id="idevice-1778594720029-dt0c41kvm" class="idevice_node text" data-idevice-path="../idevices/text/" data-idevice-type="text" data-idevice-component-type="json" data-idevice-json-data="{&quot;ideviceId&quot;:&quot;idevice-1778594720029-dt0c41kvm&quot;}">
+<div class="exe-text-template">
+    <p><em><strong>Título: Visualización y discusión de vídeo</strong></em></p>
+<p><em><strong>Tareas: </strong></em>Ver vídeo en grupo pequeño.</p>
+<p><a href="https://padlet.com/anaarenas95/educacion-vial-1w8pm3x5eyw2ksrv/wish/XGyBQb13q3O0aL6K">https://padlet.com/anaarenas95/educacion-vial-1w8pm3x5eyw2ksrv/wish/XGyBQb13q3O0aL6K</a> </p>
+<p><em><strong>Recursos: </strong></em>Tablets o proyector, vídeos educativos.</p>
+<p><em><strong>Evaluación: </strong></em>Capacidad para recordar detalles, comprensión del mensaje de seguridad vial, interacción con los compañeros.</p>
+<p><em><strong>Desarrollo:</strong></em> Se mostraría el vídeo educativo en grupo en el proyector o PDI, y después se les dejaría ver a cada uno de manera individual. Luego, dirigiría un diálogo sencillo para que los niños compartan lo que vieron y entendieron sobre seguridad vial. Durante la actividad, valoraría su capacidad para recordar detalles, entender el mensaje y participar con sus compañeros. </p>
+<p><br /></p>
+</div>
+</div>
+</div>
+</article>
+</div></main><div class="nav-buttons">
+<a href="../html/sesion-3.html" title="Anterior" class="nav-button nav-button-left"><span>Anterior</span></a>
+<a href="../html/sesion-5.html" title="Siguiente" class="nav-button nav-button-right"><span>Siguiente</span></a>
+</div>
+<footer id="siteFooter"><div id="siteFooterContent"> <div id="packageLicense" class="cc cc-by-sa"> <p> <span class="license-label">Licencia: </span><a href="https://creativecommons.org/licenses/by-sa/4.0/" class="license">Creative Commons: Reconocimiento - compartir igual 4.0</a></p>
+</div>
+</div></footer>
+</div>
+<p id="made-with-eXe"> <a href="https://exelearning.net/" target="_blank" rel="noopener"> <span>Creado con eXeLearning <span>(nueva ventana)</span></span></a></p>
+</body>
+</html>
